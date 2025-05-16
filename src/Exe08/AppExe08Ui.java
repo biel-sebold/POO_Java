e0c0a16793f057c49288c25c44764a09f4fb8584
Biel <157547002+biel-sebold@users.noreply.github.com>
2025-05-16 20:59:13 -0300

    .

--- src/Exe08/AppExe08Ui.java
+++ src/Exe08/AppExe08Ui.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package Exe08;
 
-/**
- *
- * @author Almir
- */
 public interface AppExe08Ui {
+    
+    
     
 }
